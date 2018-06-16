2ab212f953107ea47d3a5380d0bda0e5822ccd00
Nevena Stankovic <s.nevena@outlook.com>
2018-06-16 12:33:05 +0200

    q

--- ProjekatAzil/Controllers/DogsController.cs
+++ ProjekatAzil/Controllers/DogsController.cs
@@ -218,7 +218,7 @@ namespace ProjekatAzil.Controllers
             }
         }
 
-        public ActionResult DeleteImage(int dogId, int imageId) //qwe
+        public ActionResult DeleteImage(int dogId, int imageId) //qwebfhasdgfyug
         {
             var dog = db.Dogs.FirstOrDefault(d => d.Id == dogId);
             var image = db.Images.FirstOrDefault(i => i.Id == imageId);

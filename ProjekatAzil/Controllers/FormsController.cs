1609d1d0aecf240e6464a2ce145fde7fac58e8cf
Milica Prvulovic <milica.prvulovic90@gmail.com>
2018-06-18 21:01:17 +0200

    Added events

---
+++ ProjekatAzil/Controllers/FormsController.cs
@@ -0,0 +1,30 @@
+﻿using ProjekatAzil.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ProjekatAzil.Controllers
+{
+    public class FormsController : Controller
+    {
+        // GET: Forms
+        public ActionResult Index(Days[] days)
+        {
+
+            if (days != null && days.Count() > 0)
+            {
+                var test = (Days)days.Sum(d => (int)d);
+
+                VolunteerForm vf = new VolunteerForm()
+                {
+                    Days = test
+                };
+            }
+
+
+            return View("VolunteerForm");
+        }
+    }
+}
---
+++ ProjekatAzil/Models/Form.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace ProjekatAzil.Models
+{
+    public class Form
+    {
+       public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Address { get; set; }
+        public int PhoneNumber { get; set; }
+        public string Coment { get; set; }
+    }
+    public enum Services
+    {
+        [Display(Name ="gjuuk")]
+        DogWalking,
+        DogCare,
+        ShelterMaintenance,
+        Anything
+    }
+
+    [Flags]
+    public enum Days
+    {
+        Monday = 0,
+        Thuseday = 1,
+        Wednesday = 2,
+        Thursday = 4,
+        Friday = 8,
+        Saturday = 16,
+        Sunday = 32
+    }
+    
+
+   
+
+    public class VolunteerForm : Form
+    {
+        
+        public Services Services { get; set; }
+        public Days Days { get; set; } 
+
+    }
+}
